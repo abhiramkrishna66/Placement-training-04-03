a6d6cc6609cc19551016c8841a2b230f2153dc93
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-03-04 16:01:38 +0530

    Create AlphaRepetition.java

---
+++ AlphaRepetition.java
@@ -0,0 +1,25 @@
+public class AlphaRepetition {
+    public static void main(String[] args) {
+        String str = "t6hj7ui";
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                result.append(ch);
+            } else if (Character.isDigit(ch)) {
+                int count = Character.getNumericValue(ch);
+                if (result.length() > 0) {
+                    char prevChar = result.charAt(result.length() - 1);
+
+                    for (int j = 1; j < count; j++) {
+                        result.append(prevChar);
+                    }
+                }
+            }
+        }
+
+        System.out.print(result.toString());
+    }
+}
